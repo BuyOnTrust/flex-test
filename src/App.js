8a7d092fa77d89174a0421b0fdd0b844e7bb4e64
Weston Gibler <weston@bluekeel.com>
2022-01-07 13:27:06 -0400

    Flex shopper setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	useEffect(() => {
+		window.FlexSDK.Button({
+			createOrder (data, actions) {
+				return actions.transaction.create({
+					transactionId: "ABC-129384",
+					items: [
+						{
+							description: "Macbook Pro 13",
+							sku: "ABC123",
+							cost: 120.34,
+							brand: "Apple",
+							condition: "new",
+							quantity: 1,
+							images: [ // optional
+								"https://images.dog.ceo/breeds/husky/n02110185_11635.jpg" 
+							],
+							shipping: {
+								cost: 10.33,
+								date: (new Date()).toString(),
+								method: "ground"
+							}
+						}
+					]
+				})
+				},
+				onDecision (data) {
+					const decisionData = atob(data);
+					console.log({ decisionData });
+				}
+		}).render('#flex-shopper');
+	},[]);
+
+	return (
+		<div className="App">
+			<div style={{ minHeight: '100%', width: '300px' }} id='flex-shopper'></div>
+		</div>
+	);
 }
 
 export default App;
